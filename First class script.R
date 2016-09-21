e213fa5c7ad32bade4b5a6a3e9faa418df1695b8
jhanna06 <jhanna06@uoguelph.ca>
2016-09-21 12:08:51 -0400

    Add files via upload
    Added the files for the first markdown assignment

---
+++ First class script.R
@@ -0,0 +1,23 @@
+install.packages("rmarkdown", dependencies = TRUE)
+install.packages("knitr", dependencies = TRUE)
+install.packages("reshape2", dep = TRUE)
+install.packages("stringi", dep= TRUE)
+install.packages("stringi", dep= TRUE)
+install.packages("apaTables", dep=TRUE)
+install.packages("evaluate", dep = TRUE)
+install.packages("corrplot", dep = TRUE)
+library(rmarkdown)
+library(knitr)
+library(reshape2)
+library(ggplot2)
+library(stringi)
+library(apaTables)
+library(evaluate)
+library(corrplot)
+
+
+#str(CSVFILE)
+#?as.integer
+#?aes
+#?rep
+
